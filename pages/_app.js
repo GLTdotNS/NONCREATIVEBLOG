25aa0d3882a016eb5d94ea5b22a9cd00a178733a
GLTdotNS <georgitonkov94@gmail.com>
2024-06-22 15:51:05 +0200

    add files

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,7 @@ import Loading from "../components/Loading/Loading";
 import Head from "next/head";
 import CookieBanner from "../components/Cookies/cookies";
 import MyContext, { MyContextProvider } from "../Context/context";
+import Head from "next/head";
 function MyApp({ Component, pageProps }) {
   const [loading, setLoading] = useState(false);
 
@@ -43,6 +44,41 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
+      <Head>
+        <title>
+          Блог на Георги Тонков - Скандинавска кухня, лични истории и забавления
+        </title>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="title"
+          content="NONCREATIVEBLOG/Блог на Георги Тонков - Скандинавска митология,модерна кухн лични истории и забавни неща."
+        />
+
+        <meta
+          name="description"
+          content="Разгледайте скандинавската митология и друга гледна точка.Запознайте се с модерната кухня и лесни рецепти за дома и офис. Лични истории и забавни мемета в блога на Георги Тонков."
+        />
+        <meta name="author" content="Georgi Tonkov" />
+        <meta name="language" content="BG" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://noncreativeblog.net/" />
+        <meta
+          property="og:title"
+          content="NONCREATIVEBLOG/Блог на Георги Тонков - Скандинавска митология, лични истории и забавления"
+        />
+        <meta
+          property="og:description"
+          content="Разгледайте скандинавската митология и друга гледна точка.Запознайте се с модерната кухня и лесни рецепти за дома и офис. Лични истории и забавни мемета в блога на Георги Тонков."
+        />
+
+        <meta property="og:locale" content="bg_BG" />
+
+        <link rel="canonical" href="https://noncreativeblog.net/" />
+
+        <link rel="icon" href="/favicon.ico" />
+      </Head>{" "}
       {loading ? (
         <Loading />
       ) : (
--- pages/blog.js
+++ pages/blog.js
@@ -70,41 +70,6 @@ export function Blog({ posts, initialCategory, author }) {
   return (
     <>
       {" "}
-      <Head>
-        <title>
-          Блог на Георги Тонков - Скандинавска кухня, лични истории и забавления
-        </title>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta
-          name="title"
-          content="NONCREATIVEBLOG/Блог на Георги Тонков - Скандинавска митология,модерна кухн лични истории и забавни неща."
-        />
-
-        <meta
-          name="description"
-          content="Разгледайте скандинавската митология и друга гледна точка.Запознайте се с модерната кухня и лесни рецепти за дома и офис. Лични истории и забавни мемета в блога на Георги Тонков."
-        />
-        <meta name="author" content="Georgi Tonkov" />
-        <meta name="language" content="BG" />
-
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://noncreativeblog.net/" />
-        <meta
-          property="og:title"
-          content="NONCREATIVEBLOG/Блог на Георги Тонков - Скандинавска митология, лични истории и забавления"
-        />
-        <meta
-          property="og:description"
-          content="Разгледайте скандинавската митология и друга гледна точка.Запознайте се с модерната кухня и лесни рецепти за дома и офис. Лични истории и забавни мемета в блога на Георги Тонков."
-        />
-
-        <meta property="og:locale" content="bg_BG" />
-
-        <link rel="canonical" href="https://noncreativeblog.net/" />
-
-        <link rel="icon" href="/favicon.ico" />
-      </Head>{" "}
       <div className="   mx-auto ">
         <div class="relative w-full absolute top-0 mx-auto mt-4 lg:mt-16">
           <Image
