4e19a7d1d202b68e078769b9cd8625237d0fa5eb
GLTdotNS <georgitonkov94@gmail.com>
2024-06-30 11:24:23 +0200

    add files

--- pages/_app.js
+++ pages/_app.js
@@ -76,6 +76,12 @@ function MyApp({ Component, pageProps }) {
           property="og:description"
           content="Разгледайте скандинавската митология и друга гледна точка.Запознайте се с модерната кухня и лесни рецепти за дома и офис. Лични истории и забавни мемета в блога на Георги Тонков."
         />
+        <meta
+          property="og:image"
+          content="https://noncreativeblog.net/images/cover.png"
+        />
+        <meta property="og:url" content="https://noncreativeblog.net" />
+        <meta property="og:type" content="website" />
 
         <meta property="og:locale" content="bg_BG" />
 
